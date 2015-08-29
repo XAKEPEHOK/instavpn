b0671020dc65d676d959765a9db97d05e38e8b09
XAKEPEHOK <icq-475204@ya.ru>
2015-08-29 12:30:33 +0300

    Eth1

--- files/setup-vpn.sh
+++ files/setup-vpn.sh
@@ -3,7 +3,7 @@ IFS=' ' read -a IPARRAY <<< $(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\(
 SERVERIP=${IPARRAY[0]}
 
 for vpn in /proc/sys/net/ipv4/conf/*; do echo 0 > $vpn/accept_redirects; echo 0 > $vpn/send_redirects; done
-iptables -t nat -A POSTROUTING -s 172.16.1.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.16.1.0/24 -o eth1 -j MASQUERADE
 iptables -t nat -A POSTROUTING -j SNAT --to-source $SERVERIP -o eth+
 
 ipsec_conf=$(cat /etc/ipsec.conf.template)
